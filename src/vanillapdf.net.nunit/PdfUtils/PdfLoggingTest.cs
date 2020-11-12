041dfae5535b94fd37a34ba675ffa3d5753710e0
Juraj Zikmund <jur.zikmund@gmail.com>
2020-11-12 20:56:20 +0100

    Logging test

---
+++ src/vanillapdf.net.nunit/PdfUtils/PdfLoggingTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace vanillapdf.net.nunit.PdfUtils
+{
+    [TestFixture]
+    public class PdfLoggingTest
+    {
+        [Test]
+        public void TestInterface()
+        {
+            PdfLogging.SetSeverity(LoggingSeverity.Debug);
+            Assert.AreEqual(LoggingSeverity.Debug, PdfLogging.GetSeverity());
+
+            PdfLogging.Enable();
+            Assert.AreEqual(true, PdfLogging.IsEnabled());
+
+            PdfLogging.Disable();
+            Assert.AreEqual(false, PdfLogging.IsEnabled());
+        }
+    }
+}
