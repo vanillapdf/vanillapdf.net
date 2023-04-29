01b43db53e32513626f7e3bc2c8ba1733114fd8a
Juraj Zikmund <jur.zikmund@gmail.com>
2023-04-30 00:02:29 +0200

    Add missing page tree functions

--- src/vanillapdf.net/PdfSemantics/PdfPageTree.cs
+++ src/vanillapdf.net/PdfSemantics/PdfPageTree.cs
@@ -54,6 +54,30 @@ namespace vanillapdf.net.PdfSemantics
             return new PdfPageObject(data);
         }
 
+        public void InsertPage(UInt64 index, PdfPageObject data)
+        {
+            UInt32 result = NativeMethods.PageTree_InsertPage(Handle, new UIntPtr(index), data.Handle);
+            if (result != PdfReturnValues.ERROR_SUCCESS) {
+                throw PdfErrors.GetLastErrorException();
+            }
+        }
+
+        public void AppendPage(PdfPageObject data)
+        {
+            UInt32 result = NativeMethods.PageTree_AppendPage(Handle, data.Handle);
+            if (result != PdfReturnValues.ERROR_SUCCESS) {
+                throw PdfErrors.GetLastErrorException();
+            }
+        }
+
+        public void RemovePage(UInt64 index)
+        {
+            UInt32 result = NativeMethods.PageTree_RemovePage(Handle, new UIntPtr(index));
+            if (result != PdfReturnValues.ERROR_SUCCESS) {
+                throw PdfErrors.GetLastErrorException();
+            }
+        }
+
         private protected override void DisposeCustomHandle()
         {
             base.DisposeCustomHandle();
@@ -64,12 +88,24 @@ namespace vanillapdf.net.PdfSemantics
         {
             public static PageTreeGetPageDelgate PageTree_GetPage = LibraryInstance.GetFunction<PageTreeGetPageDelgate>("PageTree_GetPage");
             public static PageTreeGetPageCountDelgate PageTree_GetPageCount = LibraryInstance.GetFunction<PageTreeGetPageCountDelgate>("PageTree_GetPageCount");
+            public static InsertPageDelgate PageTree_InsertPage = LibraryInstance.GetFunction<InsertPageDelgate>("PageTree_InsertPage");
+            public static AppendPageDelgate PageTree_AppendPage = LibraryInstance.GetFunction<AppendPageDelgate>("PageTree_AppendPage");
+            public static RemovePageDelgate PageTree_RemovePage = LibraryInstance.GetFunction<RemovePageDelgate>("PageTree_RemovePage");
 
             [UnmanagedFunctionPointer(MiscUtils.LibraryCallingConvention)]
             public delegate UInt32 PageTreeGetPageDelgate(PdfPageTreeSafeHandle handle, UIntPtr at, out PdfPageObjectSafeHandle data);
 
             [UnmanagedFunctionPointer(MiscUtils.LibraryCallingConvention)]
             public delegate UInt32 PageTreeGetPageCountDelgate(PdfPageTreeSafeHandle handle, out UIntPtr count);
+
+            [UnmanagedFunctionPointer(MiscUtils.LibraryCallingConvention)]
+            public delegate UInt32 InsertPageDelgate(PdfPageTreeSafeHandle handle, UIntPtr at, PdfPageObjectSafeHandle data);
+
+            [UnmanagedFunctionPointer(MiscUtils.LibraryCallingConvention)]
+            public delegate UInt32 AppendPageDelgate(PdfPageTreeSafeHandle handle, PdfPageObjectSafeHandle data);
+
+            [UnmanagedFunctionPointer(MiscUtils.LibraryCallingConvention)]
+            public delegate UInt32 RemovePageDelgate(PdfPageTreeSafeHandle handle, UIntPtr at);
         }
     }
 }
