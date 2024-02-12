d51bfec3e5a28d5aab94828551f4471cad8b3d63
Juraj Zikmund <jur.zikmund@gmail.com>
2024-02-12 22:54:47 +0100

    NEW: PdfNameObject CreateFrom Encoded/Decoded

--- src/vanillapdf.net/PdfSyntax/PdfNameObject.cs
+++ src/vanillapdf.net/PdfSyntax/PdfNameObject.cs
@@ -44,6 +44,26 @@ namespace vanillapdf.net.PdfSyntax
             return new PdfNameObject(data);
         }
 
+        public static PdfNameObject CreateFromEncodedString(string data)
+        {
+            UInt32 result = NativeMethods.NameObject_CreateFromEncodedString(data, out var handle);
+            if (result != PdfReturnValues.ERROR_SUCCESS) {
+                throw PdfErrors.GetLastErrorException();
+            }
+
+            return new PdfNameObject(handle);
+        }
+
+        public static PdfNameObject CreateFromDecodedString(string data)
+        {
+            UInt32 result = NativeMethods.NameObject_CreateFromDecodedString(data, out var handle);
+            if (result != PdfReturnValues.ERROR_SUCCESS) {
+                throw PdfErrors.GetLastErrorException();
+            }
+
+            return new PdfNameObject(handle);
+        }
+
         private PdfBuffer GetValue()
         {
             UInt32 result = NativeMethods.NameObject_GetValue(Handle, out var data);
@@ -88,9 +108,7 @@ namespace vanillapdf.net.PdfSyntax
 
         public static implicit operator PdfNameObject(string data)
         {
-            PdfNameObject result = PdfNameObject.Create();
-            result.Value.StringData = data;
-            return result;
+            return CreateFromDecodedString(data);
         }
 
         internal override PdfObject ConvertTo<T>()
@@ -139,6 +157,8 @@ namespace vanillapdf.net.PdfSyntax
         private static class NativeMethods
         {
             public static CreateDelgate NameObject_Create = LibraryInstance.GetFunction<CreateDelgate>("NameObject_Create");
+            public static CreateFromEncodedStringDelgate NameObject_CreateFromEncodedString = LibraryInstance.GetFunction<CreateFromEncodedStringDelgate>("NameObject_CreateFromEncodedString");
+            public static CreateFromDecodedStringDelgate NameObject_CreateFromDecodedString = LibraryInstance.GetFunction<CreateFromDecodedStringDelgate>("NameObject_CreateFromDecodedString");
             public static GetValueDelgate NameObject_GetValue = LibraryInstance.GetFunction<GetValueDelgate>("NameObject_GetValue");
             public static SetValueDelgate NameObject_SetValue = LibraryInstance.GetFunction<SetValueDelgate>("NameObject_SetValue");
             public static EqualsDelgate NameObject_Equals = LibraryInstance.GetFunction<EqualsDelgate>("NameObject_Equals");
@@ -146,6 +166,12 @@ namespace vanillapdf.net.PdfSyntax
 
             [UnmanagedFunctionPointer(MiscUtils.LibraryCallingConvention)]
             public delegate UInt32 CreateDelgate(out PdfNameObjectSafeHandle handle);
+
+            [UnmanagedFunctionPointer(MiscUtils.LibraryCallingConvention)]
+            public delegate UInt32 CreateFromEncodedStringDelgate(string data, out PdfNameObjectSafeHandle handle);
+
+            [UnmanagedFunctionPointer(MiscUtils.LibraryCallingConvention)]
+            public delegate UInt32 CreateFromDecodedStringDelgate(string data, out PdfNameObjectSafeHandle handle);
 
             [UnmanagedFunctionPointer(MiscUtils.LibraryCallingConvention)]
             public delegate UInt32 GetValueDelgate(PdfNameObjectSafeHandle handle, out PdfBufferSafeHandle data);
